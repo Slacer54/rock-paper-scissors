348958071d206fd828b0f1ed205387fc419cf4f4
Samuel Phillips <samuelphillips16@outlook.com>
2023-11-16 18:04:14 +0000

    Plan getComputerChoice function
    Add pseudocode for the first function which randomly gets the
    computers choice.

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function getComputerChoice {
+    // Randomly choose number from 1 to 3. Store result in variable choice.
+    // If randomNumber is 1, return rock
+    // If randomNumber is 2, return to paper
+    // If randomNumber is 3, return to scissors 
+}
