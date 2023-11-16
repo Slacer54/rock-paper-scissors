8894d8e715705a900f3414457c0266bf1902d15d
Samuel Phillips <samuelphillips16@outlook.com>
2023-11-16 18:21:17 +0000

    Add if statements to getComputerChoice
    Function should now return rock, paper or scissors based on the number
    generated and stored in choice.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,17 @@ function getComputerChoice() {
     // Randomly choose number from 1 to 3. Store result in variable choice.
     let choice = Math.floor(Math.random() * 3) + 1;
     console.log(choice);
-    // If randomNumber is 1, return rock
-    // If randomNumber is 2, return to paper
-    // If randomNumber is 3, return to scissors 
-}
+    // If choice is 1, return rock
+    // If choice is 2, return to paper
+    // If choice is 3, return to scissors 
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
 
-getComputerChoice();
+}
+console.log(getComputerChoice());
