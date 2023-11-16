bb92ba15b5d2eaeb0fd8b70cfc45c9e90be430bb
Samuel Phillips <samuelphillips16@outlook.com>
2023-11-16 18:13:05 +0000

    Add random number generation to getComputerChoice
    The code should generate a number from 1 - 3. Tested using console.log

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-function getComputerChoice {
+function getComputerChoice() {
     // Randomly choose number from 1 to 3. Store result in variable choice.
+    let choice = Math.floor(Math.random() * 3) + 1;
+    console.log(choice);
     // If randomNumber is 1, return rock
     // If randomNumber is 2, return to paper
     // If randomNumber is 3, return to scissors 
 }
+
+getComputerChoice();
